8394d19eba4f29d302b490589647ba0f09c1f341
Jun Siang Cheah <jsc81@cam.ac.uk>
2017-07-31 12:39:49 +0100

    Add skeleton React Native project

---
+++ index.android.js
@@ -0,0 +1,11 @@
+/**
+ * Main View
+ */
+
+import React from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+import App from './src/App'
+
+AppRegistry.registerComponent('TorAndroidP2P', () => App);
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+export default class Root extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Hi
+                </Text>
+            </View>
+        )
+    }
+}
